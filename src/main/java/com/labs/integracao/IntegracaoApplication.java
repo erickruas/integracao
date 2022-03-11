e4381398dafdc2d0482755940bb866c01e1eeb7f
Erick Ruas <tp_edama@nutreco.com>
2022-03-11 12:47:16 -0300

    Adicionando arquivos necessários do spring boot e dependencias a serem utilizadas.

---
+++ src/main/java/com/labs/integracao/IntegracaoApplication.java
@@ -0,0 +1,13 @@
+package com.labs.integracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracaoApplication.class, args);
+	}
+
+}
