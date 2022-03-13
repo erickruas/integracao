cff4f9ed798cbd55231a72fb8c92cb28bc05b7e1
Erick Ruas <tp_edama@nutreco.com>
2022-03-13 14:28:17 -0300

    Validação da linha a ser lida do arquivo a ser lida pelo código. Tratamento de exceções na leitura do arquivo. Correção do path dos arquivos do teste e do output.

--- src/main/java/com/labs/integracao/util/FileHandler.java
+++ src/main/java/com/labs/integracao/util/FileHandler.java
@@ -5,13 +5,11 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.labs.integracao.domain.Customer;
 import com.labs.integracao.domain.Order;
 import com.labs.integracao.domain.Product;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,20 +31,30 @@ public class FileHandler {
     */
     List<Customer> Customers = new ArrayList<>();
 
-    public void readFile(){
+    public void readFile() {
 
         try {
             BufferedReader br = new BufferedReader(
-                    new FileReader("C:\\Users\\erick\\Downloads\\data_1.txt"));
+                    new FileReader("data_1.txt"));
             String line;
+            int indexLine = 1;
+            while ((line = br.readLine()) != null) {
 
-            while((line = br.readLine()) != null) {
-                Customer lineCustomer = getCustomerFromLine(line);
-                addToList(lineCustomer);
+                if (isAValidLine(line)) {
+                    Customer lineCustomer = getCustomerFromLine(line);
+                    if (lineCustomer != null) {
+                        addToList(lineCustomer);
+                    } else {
+                        System.out.println("Invalid content in line: " + indexLine);
+                    }
+                } else {
+                    System.out.println("Invalid line size " + indexLine);
+                }
+                indexLine++;
             }
             br.close();
 
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -58,7 +66,7 @@ public class FileHandler {
          DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
          mapper.setDateFormat(dateFormat);
          try {
-             mapper.writeValue(new File("C:\\Users\\erick\\Downloads\\JSON.txt"), Customers);
+             mapper.writeValue(new File("result.json"), Customers);
          }
          catch (IOException e) {
              e.printStackTrace();
@@ -67,12 +75,19 @@ public class FileHandler {
 
     private Customer getCustomerFromLine(String line){
 
-        Customer customer = new Customer(getUserIdSubstring(line),getUserNameSubstring(line));
-        Order order = new Order(getOrderIdSubstring(line),getDateOrderSubstring(line));
-        Product product = new Product(getProductIdSubstring(line),new BigDecimal(getValorSubstring(line)));
-        order.addProduct(product);
-        customer.addOrder(order);
-        return customer;
+        try{
+            Customer customer = new Customer(getUserIdSubstring(line),getUserNameSubstring(line));
+            Order order = new Order(getOrderIdSubstring(line),getDateOrderSubstring(line));
+            Product product = new Product(getProductIdSubstring(line),new BigDecimal(getValorSubstring(line)));
+            order.addProduct(product);
+            customer.addOrder(order);
+            return customer;
+        } catch (NumberFormatException exception){
+            exception.printStackTrace();
+        } catch (DateTimeParseException exception){
+            exception.printStackTrace();
+        }
+        return null;
     }
 
     private void addToList(Customer lineCustomer){
@@ -89,6 +104,10 @@ public class FileHandler {
                 addNewProductToOrder(customerIndex, orderIndex, lineCustomer);
             }
         }
+    }
+
+    private boolean isAValidLine(String line) {
+        return (line.length() == 95 && !line.isBlank() && !line.isEmpty());
     }
 
     private int getCustomerIndex(Customer lineCustomer){
--- src/main/java/com/labs/integracao/util/Substrings.java
+++ src/main/java/com/labs/integracao/util/Substrings.java
@@ -1,5 +1,6 @@
 package com.labs.integracao.util;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -9,7 +10,11 @@ public class Substrings {
     da classe FileHandler mais limpo e organizado. */
 
     public static Long getUserIdSubstring(String line){
-        return Long.parseLong(line.substring(ArchiveConfig.USER_ID_BEGIN_INDEX, ArchiveConfig.USER_ID_END_INDEX));
+        try {
+            return Long.parseLong(line.substring(ArchiveConfig.USER_ID_BEGIN_INDEX, ArchiveConfig.USER_ID_END_INDEX));
+        }catch (NumberFormatException exception){
+            throw exception;
+        }
     }
 
     public static String getUserNameSubstring(String line){
@@ -17,20 +22,36 @@ public class Substrings {
     }
 
     public static Long getOrderIdSubstring(String line){
-        return Long.parseLong(line.substring(ArchiveConfig.ORDER_ID_BEGIN_INDEX, ArchiveConfig.ORDER_ID_END_INDEX));
+        try{
+            return Long.parseLong(line.substring(ArchiveConfig.ORDER_ID_BEGIN_INDEX, ArchiveConfig.ORDER_ID_END_INDEX));
+        }catch (NumberFormatException exception){
+            throw exception;
+        }
     }
 
     public static LocalDate getDateOrderSubstring(String line){
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyyMMdd");
-        return LocalDate.parse(line.substring(ArchiveConfig.DATE_BEGIN_INDEX, ArchiveConfig.DATE_END_INDEX), format);
+        try{
+            DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyyMMdd");
+            return LocalDate.parse(line.substring(ArchiveConfig.DATE_BEGIN_INDEX, ArchiveConfig.DATE_END_INDEX), format);
+        } catch (DateTimeException exception) {
+            throw exception;
+        }
     }
 
     public static Long getProductIdSubstring(String line){
-        return Long.parseLong(line.substring(ArchiveConfig.PRODUCT_ID_BEGIN_INDEX, ArchiveConfig.PRODUCT_ID_END_INDEX));
+        try {
+            return Long.parseLong(line.substring(ArchiveConfig.PRODUCT_ID_BEGIN_INDEX, ArchiveConfig.PRODUCT_ID_END_INDEX));
+        }catch (NumberFormatException exception){
+            throw exception;
+        }
     }
 
     public static Double getValorSubstring(String line){
-        return Double.parseDouble(line.substring(ArchiveConfig.VALUE_BEGIN_INDEX, ArchiveConfig.VALUE_END_INDEX));
+        try{
+            return Double.parseDouble(line.substring(ArchiveConfig.VALUE_BEGIN_INDEX, ArchiveConfig.VALUE_END_INDEX));
+        } catch (NumberFormatException exception){
+            throw exception;
+        }
     }
 
 }
